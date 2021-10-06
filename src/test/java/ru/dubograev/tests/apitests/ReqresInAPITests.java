4edd5cad16bcc2fa49488d42071b8df650afee02
dubograev <a.dubograev@gmail.com>
2021-10-06 16:14:36 +0300

    added API tests

--- src/test/java/ru/dubograev/tests/apitests/ReqresInAPITests.java
+++ src/test/java/ru/dubograev/tests/apitests/ReqresInAPITests.java
@@ -100,7 +100,7 @@ public class ReqresInAPITests {
     }
 
     @Test
-    @DisplayName("Put tess")
+    @DisplayName("Put test")
     public void updateUserViaPutTest() {
         given()
                 .contentType(JSON)
